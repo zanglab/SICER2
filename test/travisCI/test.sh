1b6b82a26d74f02ef73905c518dbd1e7ae4a4155
jeffreyyoo <jeffreyoo97@gmail.com>
2019-09-06 23:11:38 -0400

    CI build now actually builds from repo

--- test/travisCI/test.sh
+++ test/travisCI/test.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#First build and install SICER2
+python3 setup.py sdist
+source_dist=$(ls dist)
+pip3 install ./dist/$source_dist
 
 sicer_df -t ./test/treatment_1.bed ./test/treatment_2.bed -c ./test/control_1.bed ./test/control_2.bed -s hg38 --significant_reads
 
